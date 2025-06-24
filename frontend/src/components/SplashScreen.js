2130d713630253fcd1e1b1c37cab3dfc72c81a5c
liladhama <samadhidas108@gmail.com>
2025-06-24 19:10:17 +0400

    Splash: немного уменьшена Диана на загрузочном экране

--- frontend/src/components/SplashScreen.js
+++ frontend/src/components/SplashScreen.js
@@ -11,7 +11,7 @@ const SplashScreen = () => {
         <img src={require('../assets/welcome/heart.png')} alt="heart" className="splash-heart" style={{width: '130px', height: '130px', minWidth: '130px', minHeight: '130px', maxWidth: 'none', maxHeight: 'none', position: 'absolute', top: '140px', left: '50px'}} />
       </div>
       <div className="splash-bottom-block">
-        <img src={require('../assets/welcome/diana.png')} alt="Диана" className="splash-diana" style={{width: '400px', minWidth: '400px', height: 'auto', maxWidth: 'none', display: 'block', objectFit: 'contain', marginLeft: '110px'}} />
+        <img src={require('../assets/welcome/diana.png')} alt="Диана" className="splash-diana" style={{width: '340px', minWidth: '340px', height: 'auto', maxWidth: 'none', display: 'block', objectFit: 'contain', marginLeft: '110px'}} />
         <img src={require('../assets/welcome/yoga-mat.png')} alt="йога коврик" className="splash-yoga" style={{position: 'absolute', left: '10px', bottom: '120px', width: '110px', height: 'auto', objectFit: 'contain'}} />
       </div>
     </div>
